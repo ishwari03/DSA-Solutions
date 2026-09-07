a2f23de5e6b5dedacdf99b3174c394e44c2502e6
Ishwari Pusadkar <pusadkarishwari@gmail.com>
2026-09-07 23:14:22 +0530

    Time: 60 ms (39.13%) | Memory: 19.6 MB (43.84%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(l:int, r:int) -> bool:
+         #helper function which does palindrome check on a specific section of s
+            while l < r:
+                if s[l]!=s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+        
+        left = 0
+        right = len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                return isPalindrome(left+1,right) or isPalindrome(left,right-1)
+            left+=1
+            right-=1
+        return True
